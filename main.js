ff74aa1ae266b19744f55cb27d45625c3a5885b7
cutls <cutls@users.noreply.github.com>
2021-01-12 19:51:59 +0000

    deploy: 336f342c370847d3fad18cf38e93027a72d51b4d

--- main.js
+++ main.js
@@ -14,39 +14,9 @@
 /*!****************************!*\
   !*** ./src/css/index.scss ***!
   \****************************/
-/***/ ((module, __webpack_exports__, __webpack_require__) => {
-
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => __WEBPACK_DEFAULT_EXPORT__\n/* harmony export */ });\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../node_modules/css-loader/dist/runtime/getUrl.js */ \"./node_modules/css-loader/dist/runtime/getUrl.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_1__);\n/* harmony import */ var _img_top_png__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../img/top.png */ \"./img/top.png\");\n// Imports\n\n\n\nvar ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(function(i){return i[1]});\nvar ___CSS_LOADER_URL_REPLACEMENT_0___ = _node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_1___default()(_img_top_png__WEBPACK_IMPORTED_MODULE_2__.default);\n// Module\n___CSS_LOADER_EXPORT___.push([module.id, \"body {\\n  background-image: url(\" + ___CSS_LOADER_URL_REPLACEMENT_0___ + \");\\n  font-family: \\\"Montserrat\\\", Meiryo, Hiragino Kaku Gothic, sans-serif;\\n  background-repeat: no-repeat;\\n  background-attachment: fixed;\\n  background-position: center center;\\n  background-size: cover;\\n  padding: 0;\\n  margin: 0;\\n  overflow-x: hidden;\\n  color: white;\\n}\\n\\n#start {\\n  font-family: \\\"Open Sans\\\";\\n  display: flex;\\n  flex-wrap: wrap;\\n  justify-content: center;\\n  align-items: center;\\n  z-index: 2;\\n  height: 50vh;\\n  text-align: center;\\n  margin: 10px;\\n}\\n\\n#next {\\n  background-color: #282c37;\\n  width: 100vw;\\n  min-height: 50vh;\\n}\\n\\nh1 {\\n  font-size: 5rem;\\n  font-weight: normal;\\n}\\n\\nh2 {\\n  font-size: 3rem;\\n  font-weight: normal;\\n}\\n\\nh3 {\\n  font-size: 2rem;\\n  font-weight: normal;\\n  margin: 0;\\n}\\n\\n.white-text {\\n  color: white;\\n}\\n\\n.center {\\n  text-align: center;\\n}\\n\\np {\\n  margin: 0;\\n}\\n\\n.small-link {\\n  color: gray;\\n  transition: color 0.3s;\\n  margin-left: 3px;\\n}\\n\\n.small-link:hover {\\n  color: white;\\n}\\n\\n.spacer {\\n  height: 1rem;\\n}\\n\\n.left-side {\\n  text-align: right;\\n  width: 50vw;\\n}\\n\\n.right-side {\\n  display: flex;\\n  flex-direction: column;\\n  justify-content: center;\\n  width: 50vw;\\n}\\n\\n.flex-ct {\\n  display: flex;\\n  flex-wrap: wrap;\\n  justify-content: center;\\n}\\n.flex-ct .card {\\n  margin-left: 10px;\\n  margin-right: 10px;\\n}\\n\\n.flex-lp {\\n  display: flex;\\n  flex-wrap: wrap;\\n  justify-content: space-around;\\n}\\n\\n.card {\\n  width: 300px;\\n  max-width: 100%;\\n  min-height: 320px;\\n  margin: 5px;\\n  border-radius: 2px;\\n  box-shadow: 0 4px 5px 0 rgba(0, 0, 0, 0.14), 0 1px 10px 0 rgba(0, 0, 0, 0.12), 0 2px 4px -1px rgba(0, 0, 0, 0.3);\\n  padding: 25px;\\n  text-align: center;\\n}\\n.card .title {\\n  font-size: 2rem;\\n  margin: 10px;\\n}\\n.card .semi-title {\\n  font-size: 1.6rem;\\n  margin: 10px;\\n}\\n.card .appeal {\\n  width: 50px;\\n}\\n.card img.me {\\n  width: 20px;\\n}\\n.card .action {\\n  margin-top: 10px;\\n}\\n.card .tech {\\n  font-size: 0.7rem;\\n  color: #e1e1e1;\\n  text-align: left;\\n}\\n.card .tech .center {\\n  text-align: center;\\n}\\n.card .description {\\n  text-align: left;\\n}\\n.card .description .center {\\n  text-align: center;\\n}\\n.card .action i {\\n  font-size: 1.5rem;\\n  color: gray;\\n  margin-left: 5px;\\n  transition: color 0.3s;\\n}\\n\\n.card .action i:hover {\\n  color: white;\\n}\\n\\na {\\n  color: #7cabd1;\\n  text-decoration: none;\\n}\\na .effect {\\n  display: inline-block;\\n}\\na .effect::after {\\n  content: \\\"\\\";\\n  display: block;\\n  width: 0;\\n  transition: width 0.3s;\\n  border-bottom: 1px solid #7cabd1;\\n}\\na .effect.white-text::after {\\n  border-bottom: 1px solid #fff;\\n}\\na .effect:hover::after {\\n  width: 100%;\\n}\\n\\ndetails {\\n  margin-left: 5px;\\n}\\n\\n.pointer {\\n  cursor: pointer;\\n}\", \"\"]);\n// Exports\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);\n\n\n//# sourceURL=webpack:///./src/css/index.scss?");
-
-/***/ }),
-
-/***/ "./node_modules/css-loader/dist/runtime/api.js":
-/*!*****************************************************!*\
-  !*** ./node_modules/css-loader/dist/runtime/api.js ***!
-  \*****************************************************/
-/***/ ((module) => {
-
-eval("\n\n/*\n  MIT License http://www.opensource.org/licenses/mit-license.php\n  Author Tobias Koppers @sokra\n*/\n// css base code, injected by the css-loader\n// eslint-disable-next-line func-names\nmodule.exports = function (cssWithMappingToString) {\n  var list = []; // return the list of modules as css string\n\n  list.toString = function toString() {\n    return this.map(function (item) {\n      var content = cssWithMappingToString(item);\n\n      if (item[2]) {\n        return \"@media \".concat(item[2], \" {\").concat(content, \"}\");\n      }\n\n      return content;\n    }).join('');\n  }; // import a list of modules into the list\n  // eslint-disable-next-line func-names\n\n\n  list.i = function (modules, mediaQuery, dedupe) {\n    if (typeof modules === 'string') {\n      // eslint-disable-next-line no-param-reassign\n      modules = [[null, modules, '']];\n    }\n\n    var alreadyImportedModules = {};\n\n    if (dedupe) {\n      for (var i = 0; i < this.length; i++) {\n        // eslint-disable-next-line prefer-destructuring\n        var id = this[i][0];\n\n        if (id != null) {\n          alreadyImportedModules[id] = true;\n        }\n      }\n    }\n\n    for (var _i = 0; _i < modules.length; _i++) {\n      var item = [].concat(modules[_i]);\n\n      if (dedupe && alreadyImportedModules[item[0]]) {\n        // eslint-disable-next-line no-continue\n        continue;\n      }\n\n      if (mediaQuery) {\n        if (!item[2]) {\n          item[2] = mediaQuery;\n        } else {\n          item[2] = \"\".concat(mediaQuery, \" and \").concat(item[2]);\n        }\n      }\n\n      list.push(item);\n    }\n  };\n\n  return list;\n};\n\n//# sourceURL=webpack:///./node_modules/css-loader/dist/runtime/api.js?");
-
-/***/ }),
-
-/***/ "./node_modules/css-loader/dist/runtime/getUrl.js":
-/*!********************************************************!*\
-  !*** ./node_modules/css-loader/dist/runtime/getUrl.js ***!
-  \********************************************************/
-/***/ ((module) => {
-
-eval("\n\nmodule.exports = function (url, options) {\n  if (!options) {\n    // eslint-disable-next-line no-param-reassign\n    options = {};\n  } // eslint-disable-next-line no-underscore-dangle, no-param-reassign\n\n\n  url = url && url.__esModule ? url.default : url;\n\n  if (typeof url !== 'string') {\n    return url;\n  } // If url is already wrapped in quotes, remove them\n\n\n  if (/^['\"].*['\"]$/.test(url)) {\n    // eslint-disable-next-line no-param-reassign\n    url = url.slice(1, -1);\n  }\n\n  if (options.hash) {\n    // eslint-disable-next-line no-param-reassign\n    url += options.hash;\n  } // Should url be wrapped?\n  // See https://drafts.csswg.org/css-values-3/#urls\n\n\n  if (/[\"'() \\t\\n]/.test(url) || options.needQuotes) {\n    return \"\\\"\".concat(url.replace(/\"/g, '\\\\\"').replace(/\\n/g, '\\\\n'), \"\\\"\");\n  }\n\n  return url;\n};\n\n//# sourceURL=webpack:///./node_modules/css-loader/dist/runtime/getUrl.js?");
-
-/***/ }),
-
-/***/ "./img/top.png":
-/*!*********************!*\
-  !*** ./img/top.png ***!
-  \*********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => __WEBPACK_DEFAULT_EXPORT__\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + \"be8e3c1ecd2a497b62dcc456fab6147a.png\");\n\n//# sourceURL=webpack:///./img/top.png?");
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack:///./src/css/index.scss?");
 
 /***/ }),
 
@@ -73,7 +43,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _css
 /******/ 		}
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			id: moduleId,
+/******/ 			// no module.id needed
 /******/ 			// no module.loaded needed
 /******/ 			exports: {}
 /******/ 		};
@@ -86,35 +56,6 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _css
 /******/ 	}
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/compat get default export */
-/******/ 	(() => {
-/******/ 		// getDefaultExport function for compatibility with non-harmony modules
-/******/ 		__webpack_require__.n = (module) => {
-/******/ 			var getter = module && module.__esModule ?
-/******/ 				() => module['default'] :
-/******/ 				() => module;
-/******/ 			__webpack_require__.d(getter, { a: getter });
-/******/ 			return getter;
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/define property getters */
-/******/ 	(() => {
-/******/ 		// define getter functions for harmony exports
-/******/ 		__webpack_require__.d = (exports, definition) => {
-/******/ 			for(var key in definition) {
-/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
-/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/ 				}
-/******/ 			}
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	(() => {
-/******/ 		__webpack_require__.o = (obj, prop) => Object.prototype.hasOwnProperty.call(obj, prop)
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/make namespace object */
 /******/ 	(() => {
 /******/ 		// define __esModule on exports
@@ -124,11 +65,6 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _css
 /******/ 			}
 /******/ 			Object.defineProperty(exports, '__esModule', { value: true });
 /******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/publicPath */
-/******/ 	(() => {
-/******/ 		__webpack_require__.p = "";
 /******/ 	})();
 /******/ 	
 /************************************************************************/
